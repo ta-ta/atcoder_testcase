c70d6f4481f20b253784971938b499ada40c862c
tata <tata@tata.local>
2019-04-15 00:00:33 +0900

    howto

--- testcases.py
+++ testcases.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-python estcases.py https://atcoder.jp/contests/agc001/tasks/agc001_a A.c++
+python testcases.py https://atcoder.jp/contests/agc001/tasks/agc001_a A.c++
 '''
 
 import argparse
